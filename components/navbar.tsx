1154ee27a1d5d65812cfdc1809c37dd9ec227a95
Messef <85040583+Messef@users.noreply.github.com>
2024-07-12 18:08:38 -0400

    Init commit

---
+++ components/navbar.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import * as React from "react";
+import {
+  useState,
+  useEffect,
+  ChangeEvent,
+  SyntheticEvent,
+  UIEvent,
+} from "react";
+type NavbarPropsType = {
+    borderOne?: string;
+    borderTwo?: string;
+    borderThree?: string;
+    children?: any;
+  };
+  const Navbar = (props: NavbarPropsType) => {
+
+
+    
+  }
