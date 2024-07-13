2e0fe242d4bb1d67bf0622d5ae0943d00cb27b9a
remander <remymander@gmail.com>
2024-07-13 16:27:13 -0400

    Created the page for FAQ.

---
+++ src/app/faq/page.tsx
@@ -0,0 +1,10 @@
+import { AiOutlineDown } from "react-icons/ai";
+
+export default function Faq() {
+  return (
+    // TODO: Add Navbar
+    <div className="h-screen flex flex-col justify-center items-center">
+      
+    </div>
+  );
+}
