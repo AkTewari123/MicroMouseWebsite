36b272f09eecb0be1cf478d7c294e2eb0c352284
Messef <85040583+Messef@users.noreply.github.com>
2024-07-16 19:30:33 -0400

    Finished questions on faq page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,13 +29,13 @@ export default function Home() {
           ONE CLOCK
         </h1>
 
-        <p className="w-2/3 text-2xl font-['Outfit'] text-center mb-10">
-          Micromouse is a high-stakes competition where small mechanical mice
+        <div className="w-2/3 text-2xl font-['Outfit'] text-center mb-10">
+        The micromouse engineering competition <b className = "bg-gradient-to-tr from-[#E55D76] to-[#5FC3E4] inline-block text-transparent bg-clip-text"> (MEC)</b> is a high-stakes competition where small mechanical mice
           have to autonomously complete a maze in the least amount of time.{" "}
           <br />
           <br />
           Are you up for the challenge?
-        </p>
+        </div>
         <h1 className="text-5xl text-center mb-10">YES!</h1>
         <h1 className="text-7xl hover:text-lightBlue ease-in duration-300 animate-bounce">
           <AiOutlineDown />
