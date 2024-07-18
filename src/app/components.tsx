e9d164b0ca61fd219a6ad55affa472f617f9bc05
Messef <85040583+Messef@users.noreply.github.com>
2024-07-18 14:31:21 -0400

    Finished questions on faq page

--- src/app/components.tsx
+++ src/app/components.tsx
@@ -141,7 +141,7 @@ export const Navbar = (props: NavbarPropsType) => {
                 <path
                   stroke="currentColor"
                   strokeLinecap="round"
-                  stroke-linejoin="round"
+                  strokeLinejoin="round"
                   stroke-width="2"
                   d="M1 1h15M1 7h15M1 13h15"
                 />
